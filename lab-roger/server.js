71cea87646e1f3adced99d087df4cbd0424fba04
Roger <roger.neil.davenport@gmail.com>
2018-01-22 15:00:43 -0800

    added gitignore

---
+++ lab-roger/server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const net = require('net');
+const Client = require('./model/client');
+const cmd = require('./lib/cmd');
+
+const server = module.exports = net.createServer();
+const PORT = process.env || 3000;
+const clientPool = [];
+
+server.on('connection', function(socket) {
+
+  let client = new Client(socket);
+  clientPool.push(client);
+  clientPool.map(c => c.socket.write(`\t${client.nick} has joined the conversation\n`));
+
+  socket.data('data', function(data) {
+
+  })
+
+
+
+  socket.on('close', function() {
+    clientPool  = clientPool.filter(c => c.user !== client.user);
+    clientPool.map(c => c.socket.write(`\t${client.nic} has let the conversation\n`));
+  })
+
+  socket.on('error', function() => {
+    console.err(err);
+
+
+  })
+
+});
+
+server.listen(PORT, () => console.log(`listening on port ${PORT}`);
+
+
+
+
+
+
+
+
+
+
+
+})
