6466299bf1c9a57013ad6120990c516ad78502e6
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-22 22:45:07 -0800

    Got the server code done.

--- lab-steve/server.js
+++ lab-steve/server.js
@@ -14,25 +14,56 @@ let clientPool = [];
 server.on('connection', function(socket) {
   let client = new Client(socket);
   clientPool.push(client);
-  clientPool.map(c => c.socket.write(`\t${client.nick} has joined the game\n`));
+  clientPool.map(c => c.socket.write(`\t${client.nick} has joined the channel\n`));
 
   socket.on('data', function(data) {
-    // This is where you will abstract away to your command parser module...
-
-    // console.log('socket data', data)
-    let message = data.toString();
-    clientPool
-      .filter(c => c.user !== client.user)
-      .map(c => c.socket.write(`${client.nick}: ${message}\n`));
+    let info = cmd(data);
+    socket.emit(info.action, info);
+  });
+   
+  socket.on('list', () => {
+    let n = 0;
+    client.socket.write('\nConnected Users:');
+    clientPool.map(c => {
+      if (n % 4 === 0) client.socket.write(`\n\t`);
+      client.socket.write(`${c.nick} `);
+      n++;
+    });
+    client.socket.write('\n');
   });
 
-  socket.on('close', function() {
+  socket.on('nick', (data) => {
+    client.nick = data.nickname;
+    client.socket.write(`Changed nickname to ${client.nick}\n`);
+  });
+
+  socket.on('dm', (data) => {
+    clientPool.map(c => {
+      if (c.nick === data.who) c.socket.write(`${client.nick}: ${data.msg}\n`);
+    });
+  });
+
+  socket.on('help', (data) => {
+    client.socket.write(data.msg + '\n');
+  });
+
+  socket.on('broadcast', (data) => {
+    clientPool
+      .filter(c => c.user !== client.user)
+      .map(c => c.socket.write(`${client.nick}: ${data.msg}\n`));
+  });
+
+  socket.on('quit', () => {
+    client.socket.end();
+  });
+
+  socket.on('close', () => {
     clientPool = clientPool.filter(c => c.user !== client.user);
     clientPool.map(c => c.socket.write(`\t${client.nick} has left the channel\n`));
   });
 
-  socket.on('error', function(err) {
-    console.error(err);
+  socket.on('error', (data) => {
+    console.error(data.msg);
   });
 });
 
