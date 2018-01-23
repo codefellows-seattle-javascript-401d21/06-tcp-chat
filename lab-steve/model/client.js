fb325d4e4b93a08efcedcd46222916299807b1e0
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-22 22:46:17 -0800

    Implementd a closure to assist creating usernames

--- lab-steve/model/client.js
+++ lab-steve/model/client.js
@@ -1,9 +1,19 @@
-'use strict'
+'use strict';
 
 const uuid = require('uuid');
 
 module.exports = function(socket) {
   this.socket = socket;
-  this.nick = `user_${Math.random()}`;
+  this.nick = generateName('user_');
   this.user = uuid('uuid/v4');
-}
+};
+
+let nameClosure = () => {
+  let num = 0;
+  function genNickName(prefix) {
+    return prefix.concat(num++);
+  }
+  return genNickName;
+};
+
+let generateName = nameClosure();
