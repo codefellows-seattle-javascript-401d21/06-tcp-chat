d4f80ebe4f9bccecabab2f941684135180513a3d
Mitchell M <mitchellm1618@gmail.com>
2018-01-22 21:26:02 -0800

    most of cdm.js filled out

--- lab-mitchell/lib/cmd.js
+++ lab-mitchell/lib/cmd.js
@@ -1,2 +1,20 @@
 'use strict';
 
+module.exports = (data, connected) => {
+  let message = data.toString().slice(0, -1).split(' ');
+  if (message[0][0] === '@') {
+    if (message[0] === '@quit') {
+      return {command: 'close'};
+    } else if (message[0] === '@list') {
+      return {command: 'list'};
+    } else if (message[0] === '@nickname') {
+      if (connected.filter(c => c.nickname === message[1])) return {command: 'error', err: 'Nickname already in use'};
+      return message[2] ? {command: 'error', err: '@nickname requires a name without spaces'} : {command: 'nickname', name: message[1]};
+    } else if (message[0] === '@dm') {
+      if (connected.filter(c => c.nickname === message[1]).length) {return {command: 'dm', name: message[1], said: message.slice(2).join(' ')};
+      } else return {command: 'error', err: 'Requested user does not exist'};
+    } else {
+      return {command: 'error', err: 'Command does not exist'};
+    }
+  } else return {command: 'message', said: data}; //if not using the command character @
+};
--- lab-mitchell/server.js
+++ lab-mitchell/server.js
@@ -6,7 +6,16 @@ const cmd = require('./lib/cmd');
 
 const server = module.exports = net.createServer();
 const PORT = process.env.PORT || 3000;
-const clientPool = [];
+let clientPool = [];
+
+
+//basically add in a bunch of differet commands/events such as 
+// server.on('list', function() {}
+// server.on('quit', function() {}
+// server.on('nickname', function() {}
+// server.on('dm', function() {}
+
+// also add things to them like client.socket.write('whatever the message should be');
 
 server.on('connection', function(socket) {
   let client = new Client(socket);
@@ -14,17 +23,25 @@ server.on('connection', function(socket) {
   clientPool.map(c => c.socket.write(`\t${client.nickname} has joined the channel\n`));
 
   socket.on('data', function(data) {
+    //something about command parser module, CLI stuff with the different commands
+    let msg = cmd(data, clientPool);
+    socket.emit(msg.command, msg);
 
+    // let message = data.toString();
+    // clientPool.filter(
+    //   c => c.user !== client.user).map(
+    //     c => c.socket.write(`${client.nickname}: ${message}\n`));
   });
 
   socket.on('close', function() {
-
+    clientPool = clientPool.filter(c => c.user !== client.user);
+    clientPool.map(c => c.socket.write(`\t${client.nickname} has left the channel\n`));
   });
 
   socket.on('error', function(err) {
-
+    console.error(err);
   });
+});
 
-
-
+//turn on server to listen for commands, this line logs to nodemon when running
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
