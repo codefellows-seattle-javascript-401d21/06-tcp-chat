491dbc28bce12262b701a73170362364d7295d5a
Roger <roger.neil.davenport@gmail.com>
2018-01-22 21:06:12 -0800

    added readme file

--- lab-roger/lib/cmd.js
+++ lab-roger/lib/cmd.js
@@ -10,9 +10,13 @@ cmd.showData = function(data) {
   // console.log('str array', strArray);
   if (strArray[0] === '/quit') { dataObj.command = 'close';}
   if (strArray[0] === '/dm') {
+    let message = '';
+    for (let i = 2; i < strArray.length; i ++) {
+      message = message + ` ${strArray[i]}`;
+    }
     dataObj.command = 'dm';
     dataObj.recipient = strArray[1];
-    dataObj.message = strArray[2];
+    dataObj.message = `${message}\n`;
   }
   if (strArray[0] === '/list') { dataObj.command = 'list';}
   if (strArray[0] === '/nick') {
