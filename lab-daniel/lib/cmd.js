fc428768cadcdbf4deda4efce8718b2165ed880b
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-01-22 18:10:24 -0800

    finished testing chat server functionality added a couple little eastereggs

--- lab-daniel/lib/cmd.js
+++ lab-daniel/lib/cmd.js
@@ -4,20 +4,25 @@
 module.exports = (data, connected) => {
   let message = data.toString().slice(0, -1).split(' ');
   if (message[0][0] === '@') {
-    if (message[0] === '@quit') {
+    switch (message[0]) {
+    case '@quit':
       return {command: 'close'};
-    } else if (message[0] === '@list') {
+    case '@list':
       return {command: 'list'};
-    } else if (message[0] === '@nickname') {
+    case '@nickname':
       if (connected.filter(c => c.nick === message[1]).length) return {command: 'error', err: 'Nickname Already In Use'};
       return message[2]? {command: 'error', err: '@nickname requires a name without spaces'} : {command: 'nickname', name: message[1]};
-    } else if (message[0] === '@dm') {
+    case '@dm':
       if (connected.filter(c => c.nick === message[1]).length) return {command: 'dm', name: message[1], said: message.slice(2).join(' ')};
       else return {command: 'error', err: 'Requested user does not exist'};
-    } else {
+    case '@jeep':
+      return message[1]? {command: 'error', err: 'Jeeps cannot talk'} : {command: 'jeep'};
+    case '@facepalm':
+      return message[1]? {command: 'error', err: 'Are you facepalming or talking?'} : {command: 'facepalm'};
+    default:
       return {command: 'error', err: 'Command does not exist'};
     }
-  } else return {command: 'message', said: data};
+  } else return {command: 'message', said: message.join(' ')};
 };
 
 
--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -1,8 +1,9 @@
-'use strict'
+'use strict';
 
 const net = require('net');
 const Client = require('./model/client');
 const cmd = require('./lib/cmd');
+const ascii = require('./assets/ascii');
 
 const server = module.exports = net.createServer();
 const PORT = process.env.PORT || 3000;
@@ -11,47 +12,53 @@ let clientPool = [];
 server.on('connection', function (socket) {
   let client = new Client(socket);
   clientPool.push(client);
-  clientPool.map(c => c.socket.write(`\t${client.nick} has joined the chat\n`))
+  clientPool.map(c => c.socket.write(`\n\t${client.nick} has joined the chat\n`))
 
   socket.on('data', function (data) {
     let msg = cmd(data, clientPool);
-    console.log(msg);
     socket.emit(msg.command, msg);
   });
 
   socket.on('list', function () {
-    client.socket.write('\tConnected Users:\n');
-    clientPool.map (c =>  client.socket.write(`\t\t\t${c.nick}\n`));
+    client.socket.write('\n\tConnected Users:\n');
+    clientPool.map (c =>  client.socket.write(`\n\t\t\t${c.nick}\n`));
   });
-
+  
+  socket.on('nickname', function(data) {
+    clientPool.map(c => c.socket.write(`\n\t\t${client.nick} has changed their name to ${data.name}\n`));
+    client.nick = data.name;
+  });
+  
   socket.on('message', function (data) {
     let message = data.said;
     clientPool.filter(c => c.user !== client.user)
-      .map(c => c.socket.write(`${client.nick}: ${message}\n`));
-    client.socket.write(`You Said: ${message}\n`);
-  });
-
-  socket.on('nickname', function(data) {
-    client.nick = data.name;
-    console.log(clientPool);
+      .map(c => c.socket.write(`\n${client.nick}: ${message}\n`));
+    client.socket.write(`\nYou (${client.nick}) Said: ${message}\n`);
   });
 
   socket.on('dm', function (data) {
-    let who = clientPool.filter(c => c.nick === data.who);
-    console.log('who', who)
-    // who[0].socket.write(`Directly From ${client.nick}: ${data.said}`);
-    // client.socket.write(`Directly To ${data.who}: ${data.said}`);
+    let who = clientPool.filter(c => c.nick === data.name);
+    who[0].socket.write(`\nDirectly From ${client.nick}: ${data.said}\n`);
+    client.socket.write(`\nDirectly To ${data.name}: ${data.said}\n`);
+  });
+
+  socket.on('jeep', function () {
+    let jeep = ascii.jeep.split('%');
+    clientPool.map(c => c.socket.write(`\n${jeep[0]}${client.nick}'s A JEEP\n\n`));
+  });
+
+  socket.on('facepalm', function () {
+    clientPool.map(c => c.socket.write(`\n${client.nick} is disappointed\n\n${ascii.facepalm}`));
   });
 
   socket.on('close', function () {
     clientPool = clientPool.filter(c => c.user !== client.user);
-    clientPool.map(c => c.socket.write(`\t${client.nick} has left the chat\n`));
-
+    clientPool.map(c => c.socket.write(`\n\t${client.nick} has left the chat\n`));
+    socket.end();
   });
 
   socket.on('error', function (data) {
-    client.socket.write(`\t\t!!!\tError\t!!!:\n ${data.err}\n`);
-
+    client.socket.write(`\n\t\t!!!\tError\t!!!:\n \t\t${data.err}\n`);
   });
 });
 
