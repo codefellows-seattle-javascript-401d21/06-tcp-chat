493236e4815ea78cdaecaea5193b43d14566c017
Koko Kassa <kowserk7@gmail.com>
2018-01-22 20:30:26 -0800

    got everything up and running properly

---
+++ lab-koko/lib/cmd.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports =  (data, clientPool) => {
+  let message = data.toString().slice(0, -1).split(' ');
+  if (message [0][0] === '@') {
+    switch(message[0]){
+    case '@quit' : 
+      return {command: 'close'};
+    case '@list' : 
+      return {command: 'list'};
+    case '@nickname' : 
+      return {command: 'nickname', name: message[1]};
+    case '@dm' : 
+      if (clientPool.filter(c => c.nickname === message[1]).length) return {command: 'dm', name: message[1], said: message.slice(2).join(' ')};
+    } 
+  } else return {command: 'message', said: message.join(' ')};
+};
--- lab-koko/server.js
+++ lab-koko/server.js
@@ -3,37 +3,55 @@
 // Application dependencies
 const net = require('net');
 const Client = require('./model/client');
-// const cmd = require('./lib/cmd');
+const cmd = require('./lib/cmd');
 
 // Application setup
 const server = module.exports = net.createServer();
 const PORT = process.env.PORT || 5000;
-const clientPool = [];
+let clientPool = [];
 
 // Server instance setup
 server.on('connection', function(socket) {
   let client = new Client(socket);
   clientPool.push(client);
-  clientPool.map(c => c.socket.write(`\t${client.nick} has joined the game\n`));
+  socket.write( 'Welcome ' + client.nickname + '!\n');
+  clientPool.map(c => c.socket.write(`\t${client.nickname} has joined the conversation.\n`));
 
   socket.on('data', function(data) {
-    // This is where you will abstract away to your command parser module...
-
-    // console.log('socket data', data)
     let message = data.toString();
     clientPool.filter(
       c => c.user !== client.user).map(
-      c => c.socket.write(`${client.nick}: ${message}\n`));
+      c => c.socket.write(`${client.nickname}: ${message}\n`));
+  });
+
+  socket.on('data', function (data) {
+    let msg = cmd(data, clientPool);
+    socket.emit(msg.command, msg);
   });
 
   socket.on('close', function() {
-    let clientPool = clientPool.filter(c => c.user !== client.user);
-    clientPool.map(c => c.socket.write(`\t${client.nick} has left the channel\n`));
+    clientPool = clientPool.filter(c => c.user !== client.user);
+    clientPool.map(c => c.socket.write(`\t${client.nickname} has left the conversation.\n`));
   });
 
-  socket.on('error', function(err) {
-    console.error(err);
+  socket.on('list', function () {
+    client.socket.write('\n\tUsers:\n');
+    clientPool.map (c =>  client.socket.write(`\n\t${c.nickname}\n`));
   });
+
+  socket.on('nickname', function(data) {
+    clientPool.map(c => c.socket.write(`\n\t\t${client.nickname} has changed their name to ${data.name}\n`));
+    client.nickname = data.name;
+  });
+
+  socket.on('dm', function (data) {
+    let who = clientPool.filter(c => c.nickname === data.name);
+    who[0].socket.write(`\nFrom ${client.nickname}: ${data.said}\n`);
+    client.socket.write(`\nTo ${data.name}: ${data.said}\n`);
+  });
+
+  socket.on('error', function(data) {
+    client.socket.write(`\n${data.err}\n`);  });
 });
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
