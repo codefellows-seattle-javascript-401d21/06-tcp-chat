6b4d687a977d1e1a7088e4887e3af18c0057aa4a
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-22 21:14:29 -0800

    add working files

---
+++ lab-yohanes/client.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const uuid = require('uuid');
+
+module.exports = function (socket) {
+  this.socket = socket;
+  this.nickname = `User_${Math.floor(Math.random() * 20)}`;
+  this.user = uuid('uuid/v4');
+};
